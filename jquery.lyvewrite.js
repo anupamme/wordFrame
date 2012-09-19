fcd0ad173d67dbd2eebe3a07252d1b38fa521550
rahul <rahulsidhu@gmail.com>
2012-09-19 09:08:25 +0530

    basics in place

--- jquery.lyvewrite.js
+++ jquery.lyvewrite.js
@@ -1,14 +1,30 @@
 
 (function($) {
+  'use strict';
   
   $.fn.lyvewrite = function() {
+
+    var $menu = $('<div/>');
+
+    var render = function($el) {
+      return $el.empty()
+	.append('<a href="#" data-type="bold">Bold</a>')
+	.append('<a href="#" data-type="italic">Italic</a>')
+	.append('<a href="#" data-type="list">List</a>')
+	.append('<a href="#" data-type="link">Link</a>')
+	.append('<a href="#" data-type="h2">Large</a>')
+	.append('<a href="#" data-type="h3">Medium</a>');
+    };
 
     return this.each(function() {
       
       $(this)
 	.attr('contenteditable', true)
-	.css({'height':600, 'width': 800});
+	.addClass('area');
 
+      render($menu)
+	.addClass('lyvewrite')
+	.insertBefore($(this));
     });
     
   };
