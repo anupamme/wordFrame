1127e7d79618567a1c0891c0c7ac0e7652ac65b8
rahul <rahulsidhu@gmail.com>
2012-09-22 21:38:31 +0530

    changed structure again

--- jquery.lyvewrite.js
+++ jquery.lyvewrite.js
@@ -1,181 +1,157 @@
-
 (function ($, window, document) {
   'use strict';
 
-  var methods = (function () {
+  var getSelectionText = function () {
+    var text = "";
+    if (document.getSelection) {
+      text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type == "Text") {
+      text = document.selection.createRange().text;
+    }
+    return text;
+  },
 
-    var getSelectionText = function () {
-      var text = "";
-      if (document.getSelection) {
-        text = window.getSelection().toString();
-      } else if (document.selection && document.selection.type == "Text") {
-        text = document.selection.createRange().text;
-      }
-      return text;
-    },
-
-    selectTest = function() {
-      if (getSelectionText().length === 0) {
-	alert('Please select some text first');
-	return false;
-      } else { return true; }
-    },
-    
-    exec = function (command, arg) {
-      document.execCommand(command, false, typeof arg !== undefined ? arg : null);
-    },
-
-    query = function (command) {
-      return document.queryCommandValue(command);
-    },
-
-    bold = function (e) {
-      e.preventDefault();
-      exec('bold');
-    },
-    
-    italic = function (e) {
-      e.preventDefault();
-      exec('italic');
-    },
-    
-    list = function (e) {
-      e.preventDefault();
-      exec('insertUnorderedList');
-    },
-
-    link = function (e) {
-      e.preventDefault();
-      if (selectTest()) {
-	exec('unlink');
-	var href = prompt('Enter a link:', 'http://');
-	console.log(href);
-	exec('createLink', href);
-      } else { return; }
-    },
-    
-    h2 = function (e) {
-      e.preventDefault();
-      if (query('formatBlock') === 'h2') {
-	exec('formatBlock', 'p');
-      } else { exec('formatBlock', 'h2'); }
-    },
-    
-    h3 = function (e) {
-      e.preventDefault();
-      if (query('formatBlock') === 'h3') {
-	exec('formatBlock', 'p');
-      } else { exec('formatBlock', 'h3'); }
-    },
-
-    getContents = function (e) {
-      e.preventDefault();
-      return $
-    },
-    
-    cancel = function (e) {
-      e.preventDefault();
-      e.stopImmediatePropagation();
-    },
-
-    addTextarea = function ($el, className) {
-      var $textarea = $("<div id='lwtextarea'/>")
-	.attr('contenteditable', true)
-	.addClass(className);
-
-      return $el.append($textarea);
-    },
-    
-    addMenu = function ($el, menuItems, className) {
-      
-      var $menu = $("<div id='lwmenu'/>").addClass(className);
-
-      if (menuItems instanceof Array) {
-	menuItems.forEach(function (item, idx, array) {
-	  $menu.append(item);
-	}, null);
-      } else {
-	$.error('incorrect argument passed to function addMenu');
-      }
-      
-      return $el.prepend($menu);
-    },
-
-    delegateEvents = function ($el, eventsObj) {
-
-      for (var selector in eventsObj) {
-
-	var eventMapList = eventsObj[selector];
-	eventMapList.forEach(function (eventMap, idx, array) {
-
-	  for (var event in eventMap) {
-
-	    var handlers = eventMap[event];
-	    handlers.forEach(function (handler, idx, array) {
-	      $el.delegate(selector, event, handler);
-	    }, null);
-
-	  }
-	}, null);
-
-      }
-    },
-
-    data = {
-
-      areaClassName: 'area',
-
-      menuClassName: 'lyvewrite',
-
-      eventsObj: {
-	'[data-type=bold]'  : [ {'click': [bold]} ],
-	'[data-type=italic]': [ {'click': [italic]} ],
-	'[data-type=list]'  : [ {'click': [list]} ],
-	'[data-type=link]'  : [ {'click': [link]} ],
-	'[data-type=h2]'    : [ {'click': [h2]} ],
-	'[data-type=h3]'    : [ {'click': [h3]}	],
-	'[data-type=view]'  : [ {'click': [getContents]} ],
-	'a'                 : [ {'click': [cancel]} ]
-      },
-
-      menuItems: [
-      	'<a href="#" data-type="bold">Bold</a>',
-	'<a href="#" data-type="italic">Italic</a>',
-	'<a href="#" data-type="list">List</a>',
-	'<a href="#" data-type="link">Link</a>',
-	'<a href="#" data-type="h2">Large</a>',
-	'<a href="#" data-type="h3">Medium</a>',
-	'<a href="#" data-type="view">View</a>'
-      ]
-      
-    };
-    
-    return {
-      
-      init: function (options) {
-	
-	var settings = $.extend(data, options || {});
-	
-	return this.each(function (idx, el) {
-	  addMenu($(el), settings.menuItems, settings.menuClassName);
-	  addTextarea($(el), settings.areaClassName);
-	  delegateEvents($(el), settings.eventsObj);
-	});
-      },
-
-    };
-
-  }());
+  selectTest = function() {
+    if (getSelectionText().length === 0) {
+      alert('Please select some text first');
+      return false;
+    } else { return true; }
+  },
   
-  $.fn.lyvewrite = function (method) {
+  exec = function (command, arg) {
+    document.execCommand(command, false, typeof arg !== undefined ? arg : null);
+  },
+
+  query = function (command) {
+    return document.queryCommandValue(command);
+  },
+
+  bold = function (e) {
+    e.preventDefault();
+    exec('bold');
+  },
+  
+  italic = function (e) {
+    e.preventDefault();
+    exec('italic');
+  },
+  
+  list = function (e) {
+    e.preventDefault();
+    exec('insertUnorderedList');
+  },
+
+  link = function (e) {
+    e.preventDefault();
+    if (selectTest()) {
+      exec('unlink');
+      var href = prompt('Enter a link:', 'http://');
+      exec('createLink', href);
+    } else { return; }
+  },
+  
+  h2 = function (e) {
+    e.preventDefault();
+    if (query('formatBlock') === 'h2') {
+      exec('formatBlock', 'p');
+    } else { exec('formatBlock', 'h2'); }
+  },
+  
+  h3 = function (e) {
+    e.preventDefault();
+    if (query('formatBlock') === 'h3') {
+      exec('formatBlock', 'p');
+    } else { exec('formatBlock', 'h3'); }
+  },
+  
+  cancel = function (e) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+  },
+  
+  addMenu = function ($el, buttons, className) {
     
-    if ( methods[method] ) {
-      return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
-    } else if ( typeof method === 'object' || ! method ) {
-      return methods.init.apply( this, arguments );
+    var buttonsList = buttons.split(",");
+    var $menu = $("<div id='lwmenu'/>").addClass(className);
+  
+    if (buttonsList instanceof Array) {
+      buttonsList.forEach(function (name, idx, array) {
+        var button = "<a href='#' data-type=" + name + ">" + name + "</a>";
+        $menu.append(button);
+      }, null);
     } else {
-      $.error( 'Method ' +  method + ' does not exist on jQuery.lyvewrite' );
-    }    
+      $.error('incorrect argument passed to function addMenu');
+    }
+    
+    return $el.prepend($menu);
+  },
+
+  addTextarea = function ($el, className) {
+    var $textarea = $("<div id='lwtextarea'/>")
+      .attr('contenteditable', true)
+      .addClass(className);
+
+    return $el.append($textarea);
+  },
+  
+  delegateEvents = function ($el, eventsObj) {
+
+    for (var selector in eventsObj) {
+
+      var eventMapList = eventsObj[selector];
+      eventMapList.forEach(function (eventMap, idx, array) {
+
+  for (var event in eventMap) {
+
+	  var handlers = eventMap[event];
+	  handlers.forEach(function (handler, idx, array) {
+	    $el.delegate(selector, event, handler);
+	  }, null);
+
+	}
+      }, null);
+
+    }
+  },
+  
+  init = function (el, options) {
+    
+    options = $.extend($.lyvewrite, options || {});
+    
+    addMenu($(el), options.buttons, options.menuClassName);
+    addTextarea($(el), options.areaClassName);
+    delegateEvents($(el), options.eventsObj);
+  };
+
+  $.lyvewrite = {
+    
+    width: 400,
+    height: 400,
+    
+    buttons: "bold, italic, list, link, large, medium",
+    
+    areaClassName: 'area',
+
+    menuClassName: 'lyvewrite',
+
+    eventsObj: {
+      '[data-type=bold]'  : [ {'click': [bold]} ],
+      '[data-type=italic]': [ {'click': [italic]} ],
+      '[data-type=list]'  : [ {'click': [list]} ],
+      '[data-type=link]'  : [ {'click': [link]} ],
+      '[data-type=h2]'    : [ {'click': [h2]} ],
+      '[data-type=h3]'    : [ {'click': [h3]}  ],
+      'a'                 : [ {'click': [cancel]} ]
+    }
+    
+  };
+  
+  $.fn.lyvewrite = function (options) {
+    
+    return this.each(function (idx, el) {
+      $.data(el, 'lyvewrite', init(el, options));
+      
+    });
   };
   
 }(jQuery, window, document));
