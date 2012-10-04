2a6ed696e8ad98637cabadb1e85bc25ce6e3cfb2
rahul <rahulsidhu@gmail.com>
2012-10-05 02:49:06 +0530

    partly working

--- plugins/wordframe.symbols.js
+++ plugins/wordframe.symbols.js
@@ -1,20 +1,46 @@
-(function ($) {
+(function ($, document) {
+  'use strict'
+
+  var rndStr = function (prefix) {
+    return prefix + '_' + Math.random().toString().split('.')[1];
+  },
   
-  makeSymbolsEditor = function (e) {
-    var $editor = $('<div class="texEditor"/>')
-    e.data.$root.append($editor);
-    $('.texEditor').texEdit();
+  makeSymbolEditor = function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    
+    if ($('#symbolEditor').length===0) {
+      var $editor = $('<div id="symbolEditor"/>')
+      e.data.$root.append($editor);
+
+      var uniqueId = rndStr('symbols');
+      var symbols = '<span id=' + uniqueId + '></span>';
+      document.execCommand('insertHTML', false, symbols);
+      
+      $editor.symbolEditor({
+	'$outputArea': $('#'+uniqueId)
+      });
+      $editor
+	.prepend('<button class="btn-small btn-info">Done</button>')
+	.on('click', null, {'root':$editor},  removeSymbolEditor);
+    }
+  },
+  
+  removeSymbolEditor = function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    e.data.root.remove();
   },
   
   symbolButton = {
     name: 'symbols',
     html: '<i class="icon-plus"></i>',
-    helpText: 'Math Symbols',
+    helpText: 'Symbols',
     eventsMap: { 
-      'click': makeSymbolsEditor
-    }
+      'click': makeSymbolEditor
+    },
   };
 
-  $.wordFrame.addButton('symbols', symbolButton);
+  $.wordFrame.addButton(symbolButton);
 
-}(jQuery));
+}(jQuery, document));
