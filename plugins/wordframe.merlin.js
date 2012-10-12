ff88a93ebdd645432f1fbc1a3fc1561ece389e81
rahul <rahulsidhu@gmail.com>
2012-10-12 20:04:51 +0530

    added underscore

--- plugins/wordframe.merlin.js
+++ plugins/wordframe.merlin.js
@@ -1,3 +1,47 @@
-(function ($) {
+(function ($, document) {
+  'use strict'
   
-}(jQuery));
+  var makeMerlin = function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    
+    if ($('#merlin').length===0) {
+      var $merlin = $('<div id="merlin"/>')
+	    .css('float', 'left');
+      e.data.$root.css('float', 'left');
+
+      e.data.$root.append($merlin);
+
+      $merlin.merlin({
+	outsideClickDeselects: false
+      });
+
+      var $doneButton = $('<button class="btn-small btn-info">Done</button>')
+	    .on('click',
+		null, 
+		{'root':$merlin, 'textArea': e.data.$textArea},
+		removeSymbolEditor);
+
+      $merlin.prepend($doneButton);
+    }
+  },
+  
+  removeSymbolEditor = function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    e.data.root.remove();
+    e.data.textArea.focus();
+  },
+  
+  merlinButton = {
+    name: 'merlin',
+    html: '<i class="icon-th"></i>',
+    helpText: 'Spreadsheet',
+    eventsMap: { 
+      'click': makeMerlin
+    }
+  };
+
+  $.wordFrame.addButton(merlinButton);
+
+}(jQuery, document));
