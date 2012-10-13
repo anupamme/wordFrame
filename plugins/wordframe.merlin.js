8e60620746e09cdf0db8736c9fdefecc6640ec37
rahul <rahulsidhu@gmail.com>
2012-10-13 15:06:43 +0530

    one way bindings work with merlin

--- plugins/wordframe.merlin.js
+++ plugins/wordframe.merlin.js
@@ -1,67 +1,86 @@
 (function ($, document) {
   'use strict'
   
-  var handleInsert = function (e) {
+  var bindings = [],
 
-    e.data.textArea.focus();
+      handleInsert = function (e) {
 
-    var selected = e.data.merlin.handsontable('getSelected');
-    var cell = $.merlin.getCell(selected[0], selected[1]);
+	e.data.textArea.focus();
+	
+	var selected = e.data.merlin.handsontable('getSelected'),
+	    row = selected[0],
+	    col = selected[1],
+	    cell = $.merlin.getCell(row, col),
+	    code = $.merlin.utils.getCellCode(row, col),
+	    id = '_' + code,
+	    content = '<span id="'+ id  + '">' + cell.value  +'</span>';
 
-    var content = '<span>' + cell.value + '</span>';
-    document.execCommand('insertHTML', false, content);
+	document.execCommand('insertHTML', false, content);
+	
+	if (bindings.indexOf('id') === -1) {
+	  bindings.push(id);
+	}
+      },
 
-  },
+      updateDocument = function (row, col, input) {
 
-  makeMerlin = function (e) {
-    e.preventDefault();
-    e.stopPropagation();
-    
-    if ($('#merlin').length===0) {
-      var $merlin = $('<div id="merlin"/>')
-	    .css('float', 'left');
-      e.data.$root.css('float', 'left');
+	var code = $.merlin.utils.getCellCode(row, col),
+	    id = '_' + code;
+	if (bindings.indexOf(id) !== -1) {
+	  $('#'+id).html($.merlin.getCell(row, col).value);
+	}
+      },
+      
+      makeMerlin = function (e) {
+	e.preventDefault();
+	e.stopPropagation();
+	
+	if ($('#merlin').length===0) {
+	  var $merlin = $('<div id="merlin"/>')
+		.css('float', 'left');
+	  e.data.$root.css('float', 'left');
 
-      e.data.$root.append($merlin);
+	  e.data.$root.append($merlin);
 
-      $merlin.merlin({
-	outsideClickDeselects: false
-      });
+	  $merlin.merlin({
+	    outsideClickDeselects: false
+	  });
 
-      var $doneButton = $('<button class="btn-small btn-success">Done</button>')
-	    .on('click',
-		null, 
-		{'merlin': $merlin,  'textArea': e.data.$textArea},
-		removeSymbolEditor);
+	  var $doneButton = $('<button class="btn-small btn-success">Done</button>')
+		.on('click',
+		    null, 
+		    {'merlin': $merlin,  'textArea': e.data.$textArea},
+		    removeSymbolEditor);
 
-      var $insertButton = $('<button class="btn-small btn-primary">Insert</button>')
-	    .on('click', 
-		null,
-		{'merlin': $merlin, 'textArea': e.data.$textArea},
-		handleInsert);
+	  var $insertButton = $('<button class="btn-small btn-primary">Insert</button>')
+		.on('click', 
+		    null,
+		    {'merlin': $merlin, 'textArea': e.data.$textArea},
+		    handleInsert);
 
-      var $menu = $('<div></div>').append($insertButton).append($doneButton);
+	  var $menu = $('<div></div>').append($insertButton).append($doneButton);
 
-      $merlin.prepend($menu);
-    }
-  },
-  
-  removeSymbolEditor = function (e) {
-    e.preventDefault();
-    e.stopPropagation();
-    e.data.merlin.remove();
-    e.data.textArea.focus();
-  },
-  
-  merlinButton = {
-    name: 'merlin',
-    html: '<i class="icon-th"></i>',
-    helpText: 'Spreadsheet',
-    eventsMap: { 
-      'click': makeMerlin
-    }
-  };
+	  $merlin.prepend($menu);
+	}
+      },
+      
+      removeSymbolEditor = function (e) {
+	e.preventDefault();
+	e.stopPropagation();
+	e.data.merlin.remove();
+	e.data.textArea.focus();
+      },
+      
+      merlinButton = {
+	name: 'merlin',
+	html: '<i class="icon-th"></i>',
+	helpText: 'Spreadsheet',
+	eventsMap: { 
+	  'click': makeMerlin
+	}
+      };
 
   $.wordFrame.addButton(merlinButton);
+  $.merlin.on('cellUpdate', updateDocument);
 
 }(jQuery, document));
